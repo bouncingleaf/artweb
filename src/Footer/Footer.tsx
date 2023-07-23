6926355dc7619bd360fe65f0b5ab0d5f9a160241
Jessica Roy <2669123+bouncingleaf@users.noreply.github.com>
2023-07-23 18:28:11 -0400

    Updated to say art site

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => (
     <div>
         <hr />
-        See also: <Link to="https://www.developerleaf.com">my tech site</Link>
+        See also: <Link to="https://www.developerleaf.com">my developer site</Link>
     </div>
 );
 
