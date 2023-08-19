147105d37aa8c5b6b945822aa2503cdda9b44c1a
Jessica Roy <2669123+bouncingleaf@users.noreply.github.com>
2023-08-19 10:29:33 -0400

    Fix images

--- src/Gallery/Gallery.tsx
+++ src/Gallery/Gallery.tsx
@@ -1,60 +1,106 @@
 import PhotoAlbum from "react-photo-album";
+import img0001 from "../assets/sketchbook/book1-s1134032-0001.jpeg";
+import img0002 from "../assets/sketchbook/book1-s1134032-0002.jpeg";
+import img0003 from "../assets/sketchbook/book1-s1134032-0003.jpeg";
+import img0004 from "../assets/sketchbook/book1-s1134032-0004.jpeg";
+import img0005 from "../assets/sketchbook/book1-s1134032-0005.jpeg";
+import img0006 from "../assets/sketchbook/book1-s1134032-0006.jpeg";
+import img0007 from "../assets/sketchbook/book1-s1134032-0007.jpeg";
+import img0008 from "../assets/sketchbook/book1-s1134032-0008.jpeg";
+import img0009 from "../assets/sketchbook/book1-s1134032-0009.jpeg";
+import img0010 from "../assets/sketchbook/book1-s1134032-0010.jpeg";
+import img0011 from "../assets/sketchbook/book1-s1134032-0011.jpeg";
+import img0012 from "../assets/sketchbook/book1-s1134032-0012.jpeg";
+import img0013 from "../assets/sketchbook/book1-s1134032-0013.jpeg";
+import img0014 from "../assets/sketchbook/book1-s1134032-0014.jpeg";
+import img0015 from "../assets/sketchbook/book1-s1134032-0015.jpeg";
+import img0016 from "../assets/sketchbook/book1-s1134032-0016.jpeg";
+import img0017 from "../assets/sketchbook/book1-s1134032-0017.jpeg";
+import img0018 from "../assets/sketchbook/book1-s1134032-0018.jpeg";
+import img0019 from "../assets/sketchbook/book1-s1134032-0019.jpeg";
+import img2_01 from "../assets/sketchbook/book2-S6664862_01.jpg";
+import img2_02 from "../assets/sketchbook/book2-S6664862_02.jpg";
+import img2_03 from "../assets/sketchbook/book2-S6664862_03.jpg";
+import img2_04 from "../assets/sketchbook/book2-S6664862_04.jpg";
+import img2_05 from "../assets/sketchbook/book2-S6664862_05.jpg";
+import img2_06 from "../assets/sketchbook/book2-S6664862_06.jpg";
+import img2_07 from "../assets/sketchbook/book2-S6664862_07.jpg";
+import img2_08 from "../assets/sketchbook/book2-S6664862_08.jpg";
+import img2_09 from "../assets/sketchbook/book2-S6664862_09.jpg";
+import img2_10 from "../assets/sketchbook/book2-S6664862_10.jpg";
+import img2_11 from "../assets/sketchbook/book2-S6664862_11.jpg";
+import img2_12 from "../assets/sketchbook/book2-S6664862_12.jpg";
+import img2_13 from "../assets/sketchbook/book2-S6664862_13.jpg";
+import img2_14 from "../assets/sketchbook/book2-S6664862_14.jpg";
+import img2_15 from "../assets/sketchbook/book2-S6664862_15.jpg";
+import img2_16 from "../assets/sketchbook/book2-S6664862_16.jpg";
+import img2_17 from "../assets/sketchbook/book2-S6664862_17.jpg";
+import img2_18 from "../assets/sketchbook/book2-S6664862_18.jpg";
+import img2_19 from "../assets/sketchbook/book2-S6664862_19.jpg";
+import img6224 from "../assets/artomat/IMG_6224.jpeg";
+import img6225 from "../assets/artomat/IMG_6225.jpeg";
+import img6235 from "../assets/artomat/IMG_6235.jpeg";
+import img6176 from "../assets/artomat/creatures/IMG_6176.jpeg";
+import img6178 from "../assets/artomat/creatures/IMG_6178.jpeg";
+import img6179 from "../assets/artomat/creatures/IMG_6179.jpeg";
+import img6180 from "../assets/artomat/creatures/IMG_6180.jpeg";
+
 
 const sketch1 = [
-    { src: "../assets/sketchbook/book1-s1134032-0001.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0002.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0003.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0004.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0005.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0006.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0007.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0008.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0009.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0010.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0011.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0012.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0013.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0014.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0015.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0016.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0017.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0018.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book1-s1134032-0019.jpeg", width: 4800, height: 3600 },
+    { src: img0001, width: 4800, height: 3600 },
+    { src: img0002, width: 4800, height: 3600 },
+    { src: img0003, width: 4800, height: 3600 },
+    { src: img0004, width: 4800, height: 3600 },
+    { src: img0005, width: 4800, height: 3600 },
+    { src: img0006, width: 4800, height: 3600 },
+    { src: img0007, width: 4800, height: 3600 },
+    { src: img0008, width: 4800, height: 3600 },
+    { src: img0009, width: 4800, height: 3600 },
+    { src: img0010, width: 4800, height: 3600 },
+    { src: img0011, width: 4800, height: 3600 },
+    { src: img0012, width: 4800, height: 3600 },
+    { src: img0013, width: 4800, height: 3600 },
+    { src: img0014, width: 4800, height: 3600 },
+    { src: img0015, width: 4800, height: 3600 },
+    { src: img0016, width: 4800, height: 3600 },
+    { src: img0017, width: 4800, height: 3600 },
+    { src: img0018, width: 4800, height: 3600 },
+    { src: img0019, width: 4800, height: 3600 },
 ];
 
 const sketch2 = [
-    { src: "../assets/sketchbook/book2-S6664862_01.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_02.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_03.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_04.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_05.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_06.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_07.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_08.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_09.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_10.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_11.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_12.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_13.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_14.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_15.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_16.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_17.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_18.jpg", width: 4800, height: 3600 },
-    { src: "../assets/sketchbook/book2-S6664862_19.jpg", width: 4800, height: 3600 },
+    { src: img2_01, width: 4800, height: 3600 },
+    { src: img2_02, width: 4800, height: 3600 },
+    { src: img2_03, width: 4800, height: 3600 },
+    { src: img2_04, width: 4800, height: 3600 },
+    { src: img2_05, width: 4800, height: 3600 },
+    { src: img2_06, width: 4800, height: 3600 },
+    { src: img2_07, width: 4800, height: 3600 },
+    { src: img2_08, width: 4800, height: 3600 },
+    { src: img2_09, width: 4800, height: 3600 },
+    { src: img2_10, width: 4800, height: 3600 },
+    { src: img2_11, width: 4800, height: 3600 },
+    { src: img2_12, width: 4800, height: 3600 },
+    { src: img2_13, width: 4800, height: 3600 },
+    { src: img2_14, width: 4800, height: 3600 },
+    { src: img2_15, width: 4800, height: 3600 },
+    { src: img2_16, width: 4800, height: 3600 },
+    { src: img2_17, width: 4800, height: 3600 },
+    { src: img2_18, width: 4800, height: 3600 },
+    { src: img2_19, width: 4800, height: 3600 },
 ];
 
 const artomat = [
-    { src: "../assets/artomat/IMG_6224.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/artomat/IMG_6225.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/artomat/IMG_6235.jpeg", width: 3600, height: 3600 },
+    { src: img6224, width: 4800, height: 3600 },
+    { src: img6225, width: 4800, height: 3600 },
+    { src: img6235, width: 3600, height: 3600 },
 ];
 
 const creatures = [
-    { src: "../assets/artomat/creatures/IMG_6176.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/artomat/creatures/IMG_6178.jpeg", width: 4800, height: 3600 },
-    { src: "../assets/artomat/creatures/IMG_6179.jpeg", width: 3600, height: 3600 },
-    { src: "../assets/artomat/creatures/IMG_6180.jpeg", width: 3600, height: 3600 },
+    { src: img6176, width: 4800, height: 3600 },
+    { src: img6178, width: 4800, height: 3600 },
+    { src: img6179, width: 4800, height: 3600 },
+    { src: img6180, width: 4800, height: 3600 },
 ];
 
 
