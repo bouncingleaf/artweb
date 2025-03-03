ff2193c07fc2f6fd2fc735059ef89b430111a2c4
Jessica Roy <2669123+bouncingleaf@users.noreply.github.com>
2025-03-03 11:54:58 -0500

    Separated news content and added DEI scorecard

---
+++ src/Resist/DEIScorecard.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+const DEIScorecard = () => (
+    <div>
+        <h2>DEI Scorecard</h2>
+        <p>I've been wondering which major companies are willing to openly take a stand in favor of DEI work after the Trump election. Here's what I've found so far (3/3/2025).</p>
+        <ul>
+            <li><Link to="https://apnews.com/article/apple-dei-shareholder-proposal-annual-meeting-96637c8d4fa3d5e2a73dc59a59c2485b">Apple</Link></li>
+            <li><Link to="https://www.benjerry.com/about-us/media-center/election-outcome-statement">Ben & Jerry's</Link></li>
+            <li><Link to="https://finance.yahoo.com/news/cisco-ceo-chuck-robbins-takes-163250270.html">Cisco</Link></li>
+            <li><Link to="https://clutchpoints.com/nba/cleveland-cavaliers/cleveland-cavaliers-will-continue-to-champion-dei-amidst-country-uncertainties">Cleveland Cavaliers</Link></li>
+            <li><Link to="https://finance.yahoo.com/news/coca-cola-stands-firm-dei-214346347.html">Coca-Cola</Link>... but there are <Link to="https://buildremote.co/dei/coca-cola/">conflicting reports</Link></li>
+            <li><Link to="https://www.usatoday.com/story/money/2025/01/23/costco-dei-shareholder-proposal-rejected/77907655007/">Costco</Link></li>
+            <li><Link to="https://www.huffpost.com/entry/delta-airlines-dei_n_67a50420e4b051682f2ac1c7">Delta Air Lines</Link></li>
+            <li><Link to="https://edition.cnn.com/2025/01/25/business/elf-beauty-dei-support/index.html">e.l.f. Beauty</Link></li>
+            <li><Link to="https://www.linkedin.com/feed/update/urn:li:activity:7290755222606397440/">Francesca's</Link></li>
+            <li><Link to="https://www.corporateknights.com/leadership/some-big-banks-are-defending-dei/">Goldman Sachs</Link></li>
+            <li><Link to="https://www.corporateknights.com/leadership/some-big-banks-are-defending-dei/">J.P. Morgan</Link></li>
+            <li><Link to="https://www.cincinnati.com/story/money/2025/02/17/pg-kroger-are-sticking-with-diversity-programs-as-trump-targets-dei/78023676007/">Kroger</Link></li>
+            <li><Link to="https://www.youtube.com/watch?v=K_UBLlzI6Dk">McKinsey & Company</Link></li>
+            <li><Link to="https://buildremote.co/dei/meijer/">Meijer</Link></li>
+            <li><Link to="https://news.bloomberglaw.com/esg/apple-microsoft-defend-dei-as-big-tech-rivals-back-down">Microsoft</Link></li>
+            <li><Link to="https://www.cnbc.com/2025/01/24/heres-what-ceos-are-saying-about-dei-at-davos.html">Nasdaq</Link></li>
+            <li><Link to="https://frontofficesports.com/nfl-continues-resist-calls-roll-back-dei-efforts/">NFL</Link></li>
+            <li><Link to="https://www.cnbc.com/2025/01/24/heres-what-ceos-are-saying-about-dei-at-davos.html">Pinterest</Link></li>
+            <li><Link to="https://www.cincinnati.com/story/money/2025/02/17/pg-kroger-are-sticking-with-diversity-programs-as-trump-targets-dei/78023676007/">Procter & Gamble</Link></li>
+            <li>Salesforce - Salesforce CEO Marc Benioff said he plans to defend his employees against attacks on diversity and inclusion. “If somebody’s going to come after our employees or discriminate against them in any way, we’ll do everything we can to help them, support them.” (<Link to="https://www.yahoo.com/news/list-companies-continue-support-dei-230000577.html">source</Link>)</li>
+            <li><Link to="https://www.cnbc.com/2025/01/24/heres-what-ceos-are-saying-about-dei-at-davos.html">Scale AI</Link></li>
+            <li><Link to="https://www.esgdive.com/news/sephora-releases-diversity-dei-documentary-film/739425/">Sephora</Link></li>
+            <li><Link to="https://www.fastcompany.com/91011236/verizon-hiring-a-diverse-workforce-ceo-dei-davos-2024">Verizon</Link> - Issued Public Statement (Interview, Blog, Press) 2/25/2025</li>
+            <li><Link to="https://www.cnbc.com/2025/01/24/heres-what-ceos-are-saying-about-dei-at-davos.html">Vista Equity Partners</Link></li>
+        </ul>
+    </div>
+);
+
+export default DEIScorecard
